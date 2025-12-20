25a6a6f6580aa292a0d5387c5a803f9d3f253fb0
szvmczek <szymon.lebensztejn.essa@gmail.com>
2025-12-20 23:23:14 +0100

    add more testdata for task add action-card in index.html it directs into creating task add styles for action and footer

--- src/main/resources/db/testdata/0001_task.sql
+++ src/main/resources/db/testdata/0001_task.sql
@@ -1,2 +1,4 @@
 INSERT INTO task(title,description,done)
-VALUES ('Take a shower','Spend at least 15 minutes showering or doing smth else',false);
+VALUES ('Take a shower','Spend at least 15 minutes showering or doing smth else',false),
+       ('Do homework','Finish all homework',false),
+       ('Make up bed','Fuckass nigger fucking pussy',false);
