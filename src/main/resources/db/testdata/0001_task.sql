9e2d382c9e30d25ec1213b698e91cbb61c8ed066
szvmczek <szymon.lebensztejn.essa@gmail.com>
2026-01-20 20:46:35 +0100

    renamed old 'Task' to 'Habit' in ui

--- src/main/resources/db/testdata/0001_task.sql
+++ src/main/resources/db/testdata/0001_task.sql
@@ -1,4 +1,4 @@
 INSERT INTO habit(user_id,title,description,created_date)
 VALUES (1,'Take a shower','Spend at least 15 minutes showering or doing smth else',now()),
        (1,'Do homework','Finish all homework',now()),
-       (2,'Make up bed','Fuckass nigger fucking pussy',now());
+       (2,'Make up bed','Throw off the dust and make up the bed',now());
